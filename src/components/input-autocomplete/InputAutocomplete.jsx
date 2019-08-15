15194ded69d0ccac85202b681ccebf79964001cc
Daria Ratyńska <ratynskadaria@gmail.com>
2019-08-15 20:08:28 +0200

    create: * autocomplete search * display list with search result

--- src/components/input-autocomplete/InputAutocomplete.jsx
+++ src/components/input-autocomplete/InputAutocomplete.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./InputAutocomplete.css";
 
 const InputAutocomplete = props => {
-  //const [inputValue, setInputValue] = useState("");
+	const [suggestions, setSuggestions] = useState([]);
+	const [inputFocus, setInputFocus] = useState(false);
+	const test = ["France", "Spain", "Poland", "Germany"];
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={props.value}
-        onChange={event => props.onChange(event.target.value)}
-      />
-    </div>
-  );
+	useEffect(() => {
+		const result = test.filter(p => 
+			p.toLowerCase().includes(props.value)
+		);
+
+		setSuggestions(result);
+	}, [props.value]);
+
+	return (
+		<div>
+			<div>
+				<input
+					type="text"
+					value={props.value}
+					placeholder="e.g. France"
+					onChange={event => props.onChange(event.target.value)}
+					onFocus={() => setInputFocus(true)}
+					onBlur={() => setInputFocus(false)}
+				/>
+			</div>
+            {inputFocus &&
+				suggestions.map((key,index) => {
+					return <div key={index}>{key}</div>;
+				})}
+		</div>
+	);
 };
 
 export default InputAutocomplete;
